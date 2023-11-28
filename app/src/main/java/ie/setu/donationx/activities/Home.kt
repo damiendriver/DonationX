e497b47d998fc7f543c8efb8149b9074e763ef36
driverdamien <driverdamien@hotmail.com>
2023-11-28 00:33:31 +0000

    set up nav drawer

--- app/src/main/java/ie/setu/donationx/activities/Home.kt
+++ app/src/main/java/ie/setu/donationx/activities/Home.kt
@@ -2,11 +2,34 @@ package ie.setu.donationx.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import ie.setu.donationx.R
+import ie.setu.donationx.databinding.HomeBinding
 
 class Home : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var homeBinding : HomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        homeBinding = HomeBinding.inflate(layoutInflater)
+        setContentView(homeBinding.root)
+        drawerLayout = homeBinding.drawerLayout
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        val navView = homeBinding.navView
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
